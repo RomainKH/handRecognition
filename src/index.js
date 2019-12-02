825a43f8ad016e8e17bb2f3966f375e8c099ba40
Wizeeps <spam68130@gmail.com>
2019-12-02 23:56:00 +0100

    Hand Reco Square around

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const modelParams = {
     imageScaleFactor: 0.9,  // reduce input image size for gains in speed.
     maxNumBoxes: 5,        // maximum number of boxes to detect
     iouThreshold: 0.2,      // ioU threshold for non-max suppression
-    scoreThreshold: 0.75,    // confidence threshold for predictions.
+    scoreThreshold: 0.85,    // confidence threshold for predictions.
 }
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitgetUserMedia || navigator.mozgetUserMedia || navigator.msgetUserMedia
@@ -17,7 +17,6 @@ const video = document.querySelector('video#video'),
       canvas = document.querySelector('canvas'),
       context = canvas.getContext('2d')
 let model, indicator = document.querySelector('.hand')
-
 
 handTrack.startVideo(video).then(status => {
     if(status) {
@@ -34,22 +33,66 @@ handTrack.startVideo(video).then(status => {
     }
 })
 
-function runDetection(){
-    model.detect(video).then(predictions => {
-        // model.renderPredictions(predictions,canvas,context,video)
-        let pos = {x: 0, y: 0}, mediumX = 0, mediumY = 0
-        if(predictions.length > 0) {
-            console.log(predictions)
-            for (const hand of predictions) {
-                mediumX += hand.bbox[0]
-                mediumY += hand.bbox[1]
-            }
-            pos.x = mediumX / predictions.length
-            pos.y = mediumY / predictions.length
-            console.log(pos)
-            indicator.style.top = pos.y + 'px'
-            indicator.style.right = pos.x + 'px'
+class Hand {
+    constructor(id){
+        this.x = 0
+        this.y = 0
+        this.height = 0
+        this.width = 0
+        this.id = id
+        this.parent = document.querySelector('.container-video')
+        this.hand = document.createElement('div')
+        this.hand.classList.add('hands')
+        this.hand.classList.add('hand'+id)
+    }
+    createHand() {
+        this.parent.appendChild(this.hand)
+        console.log('create');
+        
+    }
+    moveHand(id, x, y, width, height) { 
+        if (this.id == id) {
+            this.hand.style.top = y + "px"
+            this.hand.style.right = x + "px"
+            this.hand.style.width = width + "px"
+            this.hand.style.height = height + "px"
         }
+    }
+    deleteHand() {
+        this.parent.removeChild(this.hand)
+        console.log('delete');
+        
+    }
+}
+
+let handsUp = []
+
+function runDetection(){
+    model.detect(video).then(predictions => {   
+        if (handsUp.length > predictions.length) {
+            for (const hand of handsUp) {
+                hand.deleteHand()
+            }
+            handsUp = []
+        }  
+        if (predictions.length > 0) {
+            if (handsUp.length < predictions.length) {
+                let square = new Hand(handsUp.length)
+                square.createHand(handsUp.length)
+                handsUp.push(square)
+            } 
+            for (let i = 0; i < predictions.length; i++) {
+                let hand = predictions[i]
+                let id = i
+                let x = hand.bbox[0]
+                let y = hand.bbox[1]
+                let width = hand.bbox[2]
+                let height = hand.bbox[3]
+                for (const hand of handsUp) {
+                    hand.moveHand(id, x, y, width, height)
+                }
+            }
+        }   
     })
 }
 
